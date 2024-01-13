198d31f76b1bbbd99544870ae98c6bf56b01be05
Luksonini <lukasz.jozef.gasior@gmail.com>
2024-01-13 22:46:25 +0100

    added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloApiView(APIView):
+    "test API view"
+    def get(self, request, format=None):
+        an_view = ["Uses HTTP method as (PUT, GET POST, patch, delete)",
+                   "is simmilat to traditional diango view"]
+        return Response({'message':'Siema!', 'an_apiview': an_view})
